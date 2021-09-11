290a272a9c60d29a734f2740fa4e5a42241bcb4d
DonyKr99 <87666573+DonyKr99@users.noreply.github.com>
2021-09-11 07:38:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var burger = document.getElementById("burger");
+var sideMenu = document.getElementById("side-menu");
+
+burger.addEventListener("click", openSideMenu);
+
+function openSideMenu(){
+  sideMenu.classList.toggle("translateX");
+  burger.classList.toggle("rotate");
+}
+
+var sideLinks = document.getElementsByClassName('sideLinks');
+for (var i = 0; i < sideLinks.length; i++) {
+  sideLinks[i].addEventListener("click", openSideMenu);
+}
+
+var topLinks = document.getElementsByClassName('topLinks');
+for (var i = 0; i < topLinks.length; i++) {
+    topLinks[i].addEventListener("click", clicked);
+    function clicked(){
+      for (var i = 0; i < topLinks.length; i++) {
+        topLinks[i].classList.remove("active");
+      }
+      this.classList.add("active");
+    }
+}
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0";
+}
+
+var tab = document.getElementById("pirmas");
+      tab.style.display = "block";
+      function changeHeader(target, name){
+        var tabs = document.getElementsByClassName("tab");
+        var buttons = document.getElementsByTagName("button");
+        for (var i = 0; i < tabs.length; i++){
+          tabs[i].style.display = "none";
+          buttons[i].classList.remove("active");
+        }
+        var tab = document.getElementById(name);
+        tab.style.display = "block";
+        target.currentTarget.classList.add("active");
+        console.log(name);
+      }
